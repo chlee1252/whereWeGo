b38da31eb41e012a7d520ce76a0c32e6831dddad
Marc Lee <chlee1252@gmail.com>
2020-11-01 16:06:49 +0900

    Connect with Sign In Page

--- app/where_we_go/lib/app/welcome/welcomePage.dart
+++ app/where_we_go/lib/app/welcome/welcomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:where_we_go/animation/delayedAnimation.dart';
+import 'package:where_we_go/app/sign_in/signInPage.dart';
 import 'package:where_we_go/app/welcome/customButton.dart';
 import 'package:where_we_go/app/welcome/welcomeLogo.dart';
 
@@ -10,46 +11,48 @@ class WelcomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              DelayedAnimation(
-                child: WelcomeLogo(
-                  text: "우리 어디가?",
-                  radius: 50.0,
-                  circleColor: Colors.green,
-                  textColor: Colors.green,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                DelayedAnimation(
+                  child: WelcomeLogo(
+                    text: "우리 어디가?",
+                    radius: 50.0,
+                    circleColor: Colors.green,
+                    textColor: Colors.green,
+                  ),
+                  delay: 800,
                 ),
-                delay: 800,
-              ),
-              SizedBox(
-                height: 40.0,
-              ),
-              DelayedAnimation(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    CustomButton(
-                      text: "로그인",
-                      textColor: Colors.white,
-                      color: Colors.green,
-                      onPressed: () => print("LOGIN"),
-                    ),
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    CustomButton(
-                      text: "회원가입",
-                      textColor: Colors.white,
-                      color: Colors.lightGreen,
-                      onPressed: () => print("REGISTER"),
-                    ),
-                  ],
+                SizedBox(
+                  height: 40.0,
                 ),
-                delay: 1600,
-              ),
-            ],
+                DelayedAnimation(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      CustomButton(
+                        text: "로그인",
+                        textColor: Colors.white,
+                        color: Colors.green,
+                        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => SignInPage())),
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      CustomButton(
+                        text: "회원가입",
+                        textColor: Colors.white,
+                        color: Colors.lightGreen,
+                        onPressed: () => print("REGISTER"),
+                      ),
+                    ],
+                  ),
+                  delay: 1600,
+                ),
+              ],
+            ),
           ),
         ),
       ),
